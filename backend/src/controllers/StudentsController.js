8ef7fd81623724886968c31125e42bdd65e89cac
Mateus Nunes <mateus.2006@gmail.com>
2020-04-18 11:42:09 -0300

    .

--- backend/src/controllers/StudentsController.js
+++ backend/src/controllers/StudentsController.js
@@ -1,18 +1,31 @@
 const connection = require('../database/connection');
+const { date } = require('../lib/utils')
 
 module.exports = {
   async create(request, response) {
+    console.log('x')
+    
+    //name_pers = data.name_pers;
     console.log(request.body)
-    const { name_pers, address_pers, email_pers } = request.body;
+    
+    const { name_pers, address_pers, birth_pers, city_pers, comments_pers, cpf_pers, phonewhats_pers, rg_pers, zipcode_pers } =  request.body
 
-    //const [code_pers] = 
-    await connection('person_pers').insert({
-        name_pers, 
-        address_pers,
-        email_pers
-    });
+    birth_pers = date(birth_pers).iso
+    
+    console.log(birth_pers)
 
-    return response.json({ res: "ok" });
-  }
+    await connection('person_pers').insert(
+        {name_pers, 
+         address_pers, 
+         birth_pers,
+         city_pers, 
+         comments_pers, 
+         cpf_pers, 
+         phonewhats_pers, 
+         rg_pers, 
+         zipcode_pers
+        });
 
-};
+    return response.json({name_pers})
+}
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const StudentsController = require('./controllers/StudentsController');
 const routes = express.Router();
 
 routes.post('/routes_newstudent', StudentsController.create);
+routes.get('/test',  function(req, res) {res.send('Rota Ok')});
 //routes.get('/ongs', OngController.index);
 //routes.delete('/incidents/:id', IncidentController.delete);
 
