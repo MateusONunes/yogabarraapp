4a45ca40920f9df6dce36b65751396fe75f07d68
mateus <mateus@mateus-pc>
2021-01-30 10:13:29 -0300

    23. @Service - Injeção de dependências com Spring
    https://www.udemy.com/course/springboot-essencial/learn/lecture/14472912#overview

--- backend/springboot/springacademia/src/main/java/com/yogabarra/api/Pessoa_pessController.java
+++ backend/springboot/springacademia/src/main/java/com/yogabarra/api/Pessoa_pessController.java
@@ -2,6 +2,7 @@ package com.yogabarra.api;
 
 import com.yogabarra.domain.Pessoa_pess;
 import com.yogabarra.domain.Pessoa_pessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/pessoa_pess/get")
 public class Pessoa_pessController {
+    @Autowired
 
-    private Pessoa_pessService service = new Pessoa_pessService();
+    private Pessoa_pessService service;
 
     @GetMapping()
     public List<Pessoa_pess> get(){
--- backend/springboot/springacademia/src/main/java/com/yogabarra/domain/Pessoa_pessService.java
+++ backend/springboot/springacademia/src/main/java/com/yogabarra/domain/Pessoa_pessService.java
@@ -1,8 +1,10 @@
 package com.yogabarra.domain;
 
+import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class Pessoa_pessService {
 
     public List<Pessoa_pess> getPessoa_pess(){
