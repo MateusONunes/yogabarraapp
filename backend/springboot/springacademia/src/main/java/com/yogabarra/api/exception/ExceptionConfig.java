cc2c008eed4993ff35294ef956aad31b768d2782
MateusONunes <mateus.2006@gmail.com>
2021-02-04 19:28:39 -0300

    Tratamento de excessão - Tranformar excessão 500-Internal server error para 404-Not Found 15 ms 130 B
    Aula:
    53. ExceptionHandler - parte 1
    https://www.udemy.com/course/springboot-essencial/learn/lecture/14867531#overview

---
+++ backend/springboot/springacademia/src/main/java/com/yogabarra/api/exception/ExceptionConfig.java
@@ -0,0 +1,16 @@
+package com.yogabarra.api.exception;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionConfig {
+  @ExceptionHandler({
+          EmptyResultDataAccessException.class // Internal server error
+  })
+  public ResponseEntity errorNotFound(Exception ex){
+    return ResponseEntity.notFound().build();
+  }
+}
